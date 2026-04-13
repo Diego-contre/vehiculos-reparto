42c56b429bb0e2340edb2f421119833c0ca60e68
DiegoContreras <contreras.isma2021@gmail.com>
2026-04-13 18:32:12 -0400

    update: se actualiza la clase Moto de reparto

--- src/MotoReparto.java
+++ src/MotoReparto.java
@@ -1,2 +1,26 @@
-public class MotoReparto {
+public class MotoReparto extends Vehiculo {
+    //Se agrega variable solo de caja termica
+    //las demas ya estan agregadas
+    private boolean cajaTermica;
+
+
+    public MotoReparto(String patente, String marca, String modelo, double capacidadCarga, boolean disponible) {
+        super(patente, marca, modelo, capacidadCarga);
+        this.cajaTermica = cajaTermica;
+
+    }
+    // === get ===
+    public boolean isCajaTermica() {
+        return cajaTermica;
+    }
+    // === set ===
+    public void setCajaTermica(boolean cajaTermica) {
+        this.cajaTermica = cajaTermica;
+    }
+
+    public void mostrarDetalle(){
+        super.mostrarDetalle();
+        System.out.println("Tipo: Moto de reparto");
+        System.out.println("Caja térmica: "+ (cajaTermica ? "Sí": "No"));
+    }
 }
