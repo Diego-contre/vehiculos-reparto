11d5ae85b0a0b6d4ff591601651c7d9f42b0a848
DiegoContreras <contreras.isma2021@gmail.com>
2026-04-13 15:48:52 -0400

    update folders

---
+++ src/Camion.java
@@ -0,0 +1,14 @@
+
+
+public class Camion extends Vehiculo {
+
+    public Camion(String patente, String marca, String modelo, double capacidadCarga, boolean disponible) {
+        super(patente, marca, modelo, capacidadCarga, disponible);
+    }
+
+
+
+
+
+
+}
---
+++ src/Vehiculo.java
@@ -0,0 +1,65 @@
+public class Vehiculo {
+
+    public String patente;
+    public String marca;
+    public String modelo;
+    public double capacidadCarga;
+    public boolean disponible;
+
+    // === Constructor ===
+    public Vehiculo(String patente, String marca, String modelo, double capacidadCarga, boolean disponible) {
+        this.patente = patente;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.capacidadCarga = capacidadCarga;
+        this.disponible = disponible;
+    }
+
+    // === GET ===
+    public String getPatente(){
+        return patente;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public double getCapacidadCarga(){
+        return capacidadCarga;
+    }
+    public boolean isDisponible(){
+        return disponible;
+    }
+
+    // === SET ===
+
+    public void setPatente(){
+        this.patente = patente;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setCapacidadCarga(double capacidadCarga) {
+        //verificamos que la capacidad de carga no sea negativo
+        if (capacidadCarga > 0){
+            this.capacidadCarga = capacidadCarga;
+        }
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String mostrarDetalle(){
+        return "Patente: "+ patente + " | Marca: " + marca + " | Modelo: " + modelo + " | ";
+    }
+
+
+}
